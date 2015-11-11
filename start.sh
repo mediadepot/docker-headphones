d88760fef44eae3c7935926e15a06cb01f12ec3b
Jason Kulatunga <jk17@ualberta.ca>
2015-11-11 14:32:04 -0800

    update headphones with depot user and correct ini file test.

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-if [ ! -f /srv/headphones/config/config.ini ]; then
+if [ ! -f /srv/headphones/config/headphones.ini ]; then
 	#generate the config file for the first time using cheetah
 
 	cheetah fill --oext ini --env /srv/headphones/config/headphones
+	chown -R depot:depot /srv/headphones
+
 fi
 
-/usr/bin/python /srv/headphones/app/Headphones.py \
-	--data_dir /srv/headphones/data \
-	--config_file /srv/headphones/config/headphones.ini
+su -c "/usr/bin/python /srv/headphones/app/Headphones.py --data_dir /srv/headphones/data --config_file /srv/headphones/config/headphones.ini" depot
