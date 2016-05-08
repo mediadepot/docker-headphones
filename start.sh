c870cfd1bc1b244e3cb6c0369e921dea1d61bb5d
Jason Kulatunga <jk17@ualberta.ca>
2016-05-08 11:08:02 -0700

    make sure headphones app is listening on all interfaces.

--- start.sh
+++ start.sh
@@ -15,5 +15,12 @@ if [ -z "$ADVANCED_DISABLEUPDATES" ]; then
 	cd /srv/headphones/app/ && su -c "git pull" mediadepot
 fi
 
+if [ ! -f /srv/headphones/config/headphones.ini ]; then
+	# create the base config file.
+	echo "[General]" > /srv/headphones/config/headphones.ini
+	echo "config_version = 5" >> /srv/headphones/config/headphones.ini
+	echo "http_host = 0.0.0.0" >> /srv/headphones/config/headphones.ini
+fi
+
 # run HeadPhones
 su -c "/usr/bin/python /srv/headphones/app/Headphones.py --datadir /srv/headphones/data --config /srv/headphones/config/headphones.ini" mediadepot
